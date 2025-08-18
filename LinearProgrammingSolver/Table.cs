6cbd5f689e964c1ad9e17285545a43531ce23965
Heinrich Rudman <heinierudman12@gmail.com>
2025-08-18 10:47:41 +0200

    Completed Table Class

--- LinearProgrammingSolver/Table.cs
+++ LinearProgrammingSolver/Table.cs
@@ -25,6 +25,71 @@ namespace LinearProgrammingSolver
             MinOrMaxQuestion = " ";
         }
 
+        public void BuildTable(string objLine, List<string> constLine)
+        {
+            ParseObjective(objLine);
+            ParseConstraint(constLine);
+            GenerateVariableNames();
+        }
+        
+        public void ParseObjective(string line)
+        {
+            string[] obj = line.Split(" ");
+
+            MinOrMaxQuestion += obj[0].ToLower();
+
+            for (int i = 1; i < obj.Length; i++)
+            {
+                if (obj[i].StartsWith("+") || obj[i].StartsWith("-"))
+                {
+                    ObjectiveValues.Add(double.Parse(obj[i]));
+                } else
+                {
+                    Console.WriteLine("All variable must start with a + or a -");
+                    break;
+                }
+            }
+        }
+
+        public void ParseConstraint(List<string> lines)
+        {
+            foreach (string line in lines)
+            {
+                string[] constraint = line.Split(" ");
+
+                List<double> values = new List<double>();
+
+                int i = 0;
+
+                while (i < constraint.Length && !constraint[i].Contains("<=") && constraint[i].Contains(">=") && constraint[i].Contains("="))
+                {
+                    if(constraint[i].StartsWith("+") || constraint[i].StartsWith("-") )
+                    {
+                        values.Add(double.Parse(constraint[i]));
+                    }
+                    else
+                    {
+                        Console.WriteLine("All variable must start with a + or a -");
+                        break;
+                    }
+                    i++;
+                }
+                ConstraintValues.Add(values);
+
+                string rhsValue = constraint[constraint.Length - 1];
+
+                RHSValues.Add(double.Parse(rhsValue));
+            }
+            
+        }
+
+        public void GenerateVariableNames()
+        {
+            for (int i = 0; i < ObjectiveValues.Count; i++)
+            {
+                VariableNames.Add($"x{i+1}");
+            }
+        }
 
     }
 }
