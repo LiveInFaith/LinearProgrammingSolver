6c3e13fc28ad0d7a8ab47534e739f66deb379df6
Heinrich Rudman <heinierudman12@gmail.com>
2025-08-18 10:52:56 +0200

    Need to Redo Filehandler to give column and row labels

--- LinearProgrammingSolver/FileHandler.cs
+++ LinearProgrammingSolver/FileHandler.cs
@@ -9,38 +9,16 @@ namespace LinearProgrammingSolver
 {
     public class FileHandler
     {
-        public static void WriteTable(string path, Table table)
+        public string FilePath;
+        public FileHandler(string FP)
         {
-            using (StreamWriter sw = new StreamWriter(path, append: false, encoding: Encoding.UTF8))
-            {
-                table.FillingTableValues(sw);
-            }
+            FilePath = FP;
         }
 
-        public static void AdditionalTable(string path, Table table, string title)
-        {
-            using (StreamWriter sw = new StreamWriter(path, append: true, encoding: Encoding.UTF8))
-            {
-                sw.WriteLine();
-                sw.WriteLine($"============= {title} =============");
-                table.FillingTableValues(sw);
 
-            }
-        }
 
-        public static string[] ReadAllLines(string path)
-        {
-            var lines = new System.Collections.Generic.List<string>();
-            using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
-            {
-                string line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    lines.Add(line);
-                }
-                return lines.ToArray();
-            }
-        }
+         
+
 
     }
 }
