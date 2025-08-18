b8315698d12ac4010eefa51b97e7ca5acd2ee820
Heinrich Rudman <heinierudman12@gmail.com>
2025-08-18 08:24:34 +0200

    Fixed some problem in Filehandler and Table class

--- LinearProgrammingSolver/FileHandler.cs
+++ LinearProgrammingSolver/FileHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace LinearProgrammingSolver
 {
@@ -10,7 +11,7 @@ namespace LinearProgrammingSolver
     {
         public static void WriteTable(string path, Table table)
         {
-            using (StreamWriter sw = new StreamWriter(path))
+            using (StreamWriter sw = new StreamWriter(path, append: false, encoding: Encoding.UTF8))
             {
                 table.FillingTableValues(sw);
             }
@@ -18,8 +19,9 @@ namespace LinearProgrammingSolver
 
         public static void AdditionalTable(string path, Table table, string title)
         {
-            using (StreamWriter sw = new StreamWriter(path))
+            using (StreamWriter sw = new StreamWriter(path, append: true, encoding: Encoding.UTF8))
             {
+                sw.WriteLine();
                 sw.WriteLine($"============= {title} =============");
                 table.FillingTableValues(sw);
 
@@ -28,7 +30,16 @@ namespace LinearProgrammingSolver
 
         public static string[] ReadAllLines(string path)
         {
-
+            var lines = new System.Collections.Generic.List<string>();
+            using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+                return lines.ToArray();
+            }
         }
 
     }
--- LinearProgrammingSolver/Table.cs
+++ LinearProgrammingSolver/Table.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace LinearProgrammingSolver
 {
@@ -11,20 +12,25 @@ namespace LinearProgrammingSolver
     {
 
         private readonly double[,] _Array;
-        public int Rows;
-        public int Columns;
-        public string[] RowNames;
-        public string[] ColNames;
+        public int Rows { get; }
+        public int Columns { get; }
+        public string[] RowNames { get; }
+        public string[] ColNames { get; }
 
         public Table(int rows, int cols, string[] rownames, string[] colnames)
         {
             if (rows <= 0 || cols <= 0) throw new Exception("rows and cols must be > 0");
             if (rownames == null) throw new Exception("row names cannot be null");
             if (colnames == null) throw new Exception("column names cannot be null");
+            if (rownames.Length != rows) throw new Exception("row names count must equal rows");
+            if (colnames.Length != cols) throw new Exception("column names count must equal cols");
 
             Rows = rows;
             Columns = cols;
             _Array = new double[rows, cols];
+
+            RowNames = (string[])rownames.Clone();
+            ColNames = (string[])colnames.Clone();
         }
         public void Set(int row, int col, double value) { _Array[row, col] = value; }
         public double Get(int row, int col) { return _Array[row, col]; }
@@ -49,10 +55,8 @@ namespace LinearProgrammingSolver
                     double round = Math.Round(_Array[r, c], 3);
                     txtW.Write(round.ToString("F3", CultureInfo.InvariantCulture));
                 }
-
+                txtW.WriteLine();
             }
-            txtW.WriteLine();
-
         }
     }
 }
