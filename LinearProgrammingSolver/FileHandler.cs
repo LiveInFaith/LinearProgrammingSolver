e75726be94a55bb7b3a08f0604cef76697bd5b24
Heinrich Rudman <heinierudman12@gmail.com>
2025-08-18 11:10:00 +0200

    Added WriteTableToFile

--- LinearProgrammingSolver/FileHandler.cs
+++ LinearProgrammingSolver/FileHandler.cs
@@ -36,7 +36,41 @@ namespace LinearProgrammingSolver
         }
 
 
-         
+        public void WriteTableToFile(Table table)
+        {
+            List<string> lines = new List<string>();
+
+            string VariableHeader = "Variable:\t";
+
+            foreach(string head in table.VariableNames)
+            {
+                VariableHeader += $"{head}\t";
+            }
+            VariableHeader += "RHS";
+            lines.Add(VariableHeader);
+
+            string ObjLine = $"{table.MinOrMaxQuestion}:\t\t";
+            foreach (double val in table.ObjectiveValues)
+            {
+                ObjLine += $"{val}\t";
+            }
+            lines.Add(ObjLine);
+
+            for (int i = 0; i < table.ConstraintValues.Count; i++)
+            {
+                string constraintLine = $"C{i + 1}:\t\t";
+                foreach (double coeff in table.ConstraintValues[i])
+                {
+                    constraintLine += $"{coeff}\t";
+                }
+                constraintLine += $"{table.RHSValues[i]}";
+                lines.Add(constraintLine);
+            }
+
+            File.WriteAllLines(FilePath.Replace(".txt", "_output.txt"), lines);
+
+        }
+        
 
 
     }
